e762ac72cc3982fbf05e026fdd6936619e29b442
Tega <beniyoke@gmail.com>
2024-03-23 01:18:07 +0100

    Modified friend ccontroller

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,9 +1,10 @@
 class FriendsController < ApplicationController
   before_action :set_friend, only: %i[ show edit update destroy ]
+  before_action :correct_user, only: %i[edit update destroy]
 
   # GET /friends or /friends.json
   def index
-    @friends = Friend.all
+    @friends = current_user.friends.all
   end
 
   # GET /friends/1 or /friends/1.json
@@ -12,7 +13,8 @@ class FriendsController < ApplicationController
 
   # GET /friends/new
   def new
-    @friend = Friend.new
+    # Instead of passing in the User Id from the form, we do it from here
+    @friend = current_user.friends.build
   end
 
   # GET /friends/1/edit
@@ -21,7 +23,8 @@ class FriendsController < ApplicationController
 
   # POST /friends or /friends.json
   def create
-    @friend = Friend.new(friend_params)
+    # Instead of passing in the User Id from the form, we do it from here
+    @friend = current_user.friends.build(friend_params)
 
     respond_to do |format|
       if @friend.save
@@ -47,6 +50,11 @@ class FriendsController < ApplicationController
     end
   end
 
+  def correct_user
+    @friend = current_user.friends.find_by(id: params[:id])
+    redirect_to friends_path, alert: "Not Authorized to edit this friend" if @friend.nil?
+  end
+
   # DELETE /friends/1 or /friends/1.json
   def destroy
     @friend.destroy!
@@ -65,6 +73,6 @@ class FriendsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def friend_params
-      params.require(:friend).permit(:first_name, :last_name, :email, :phone, :twitter, :user_id)
+      params.require(:friend).permit(:first_name, :last_name, :email, :phone, :twitter)
     end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  # Authenticate all users before they can access the app
+  before_action :authenticate_user!
   def index
   end
 
